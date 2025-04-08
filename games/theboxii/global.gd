34d0148b1d78b36e0055b99a60621ed0db3d0d62
Alexej "Xela" Tušl <acc@alexejtusl.cz>
2025-04-08 03:23:28 +0200

    Update global.gd

--- games/theboxii/global.gd
+++ games/theboxii/global.gd
@@ -3,7 +3,7 @@ extends Node
 var firstLoad = true
 
 func  _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Back_to_main"):
+	if event.is_action_pressed("Back_to_main") and not firstLoad:
 		Input.mouse_mode = 0
 		firstLoad = true
 		get_tree().change_scene_to_file("res://main/main.tscn")
