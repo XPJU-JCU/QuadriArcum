69bf86f4b972d7793a3181acb4413a2f26373f6c
Barboretta <bmemail@seznam.cz>
2025-04-19 22:13:26 +0200

    Settings finished
    - settings completely functional
    - the size of  those things is ABSOLUTE FUCKING BULSHIT THO

--- main/options_menu.gd
+++ main/options_menu.gd
@@ -8,22 +8,30 @@ extends Node
 @export 	var audio_index = AudioServer.get_bus_index("Master")
 @export var master_volume = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
 
-#@export var is_muted : bool = AudioServer.is_bus_mute(audio_index)
+@export var is_muted : bool
 
 func _ready():
+	#we start with english
 	language.select(0)
-	#mute button je potřeba mít mutted
-	#slider je potřeba změnit podle hodnoty volume 
+	
+	#muted button needs to be already checked, if muted 
+	is_muted = AudioServer.is_bus_mute(audio_index)
+	if is_muted:
+		muteButton.set_pressed(true)
+	
+	#slider update
+	var linear = db_to_linear(master_volume) * 100
+	volumeSlider.set_value_no_signal(linear) #linear
 
 #volume change
 func _on_volume_b_value_changed(value: float) -> void:
 	var linear_volume = value / 100.0
 	var db = linear_to_db(linear_volume)
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), db)
+	AudioServer.set_bus_volume_db(audio_index, db)
 
 #mute
 func _on_mute_b_button_down() -> void:
-	var is_muted : bool = AudioServer.is_bus_mute(audio_index)
+	is_muted = AudioServer.is_bus_mute(audio_index)
 	AudioServer.set_bus_mute(audio_index, !is_muted)
 
 #full screen - doesnt work, why tho?
@@ -31,7 +39,6 @@ func _on_full_screen_b_button_down() -> void:
 	var current_mode = DisplayServer.window_get_mode()
 	
 	if current_mode == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
-		
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
